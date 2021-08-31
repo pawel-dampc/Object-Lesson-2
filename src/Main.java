d82290e93a3189d75b9308d935753f59e3d3717b
Pawel <dampc86pawel@gmail.com>
2021-08-31 21:04:39 +0200

    Hello

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-public class Main {
+import java.util.Scanner;
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Enter your address: ");
+        Scanner scanner = new Scanner(System.in);
+        String address = scanner.nextLine();
+        System.out.println("You live at: " + address);
+    }
 }
